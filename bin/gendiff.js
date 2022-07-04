e05a727cd76b47eece80cbd77653416b90c0e536
Чайковский Прохор <kemononya@yandex.ru>
2022-07-04 17:09:50 +0300

    complete second stage

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .parse();
