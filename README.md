23ef4e53492c14e2145138bf15ef5f7f771dec4f
Чайковский Прохор <kemononya@yandex.ru>
2022-07-05 20:24:50 +0300

    complete third stage

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Kemononya/fullstack-javascript-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Kemononya/fullstack-javascript-project-lvl2/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/b41577901c2c777e7ecd/maintainability)](https://codeclimate.com/github/Kemononya/fullstack-javascript-project-lvl2/maintainability)
+
+asciinema: [plain-files](https://asciinema.org/a/ds2yj2l89yVwwWUes2wI914Qq)
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+
+const genDiff = (filepath1, filepath2) => {
+  const oldObject = JSON.parse(readFileSync(path.resolve(filepath1)));
+  const newObject = JSON.parse(readFileSync(path.resolve(filepath2)));
+
+  const partialDiff1 = Object.keys(oldObject).map((key) => {
+    if (!Object.hasOwn(newObject, key)) {
+      return [key, 'old'];
+    }
+    return (oldObject[key] === newObject[key]) ? [key, 'both'] : [key, 'dif'];
+  });
+
+  const partialDiff2 = Object.keys(newObject)
+    .filter((key) => (!Object.hasOwn(oldObject, key)))
+    .map((key) => [key, 'new']);
+
+  const sortedDiff = _.sortBy([...partialDiff1, ...partialDiff2], (([key]) => key));
+
+  const result = sortedDiff.map(([key, value]) => {
+    switch (value) {
+      case 'old':
+        return `  - ${key}: ${oldObject[key]}`;
+      case 'new':
+        return `  + ${key}: ${newObject[key]}`;
+      case 'dif':
+        return `  - ${key}: ${oldObject[key]}\n  + ${key}: ${newObject[key]}`;
+      default:
+        return `    ${key}: ${oldObject[key]}`;
+    }
+  });
+
+  return `{\n${result.join('\n')}\n}`;
+};
+
+export default genDiff;
